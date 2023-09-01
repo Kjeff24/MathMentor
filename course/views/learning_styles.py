49ad0ca5793126a6282bd94128c1a8a1a2581ffe
Kjeff24 <jeffreyarthur123@gmail.com>
2023-09-01 10:35:18 +0100

    modified class form

--- course/views/learning_styles.py
+++ course/views/learning_styles.py
@@ -14,13 +14,13 @@ def classForm(request):
     if request.method == 'POST':
         
         class_level = request.POST.get('class_level')
-        math_grade = request.POST.get('math_grade')
-        math_strength = request.POST.get('math_strength')
+        # math_grade = request.POST.get('math_grade')
+        # math_strength = request.POST.get('math_strength')
         
         # Update the fields of the user object
         user.class_level = class_level
-        user.math_grade = math_grade
-        user.math_strength = math_strength
+        # user.math_grade = math_grade
+        # user.math_strength = math_strength
         
         # Save the changes
         user.save()
